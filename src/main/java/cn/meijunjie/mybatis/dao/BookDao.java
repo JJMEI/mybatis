b7593f4838570cfef30bb7d194d5818d19331b56
meijunjie <meijunjie@le.com>
2017-07-15 00:31:33 +0800

    spring整合mybatis

--- src/main/java/cn/meijunjie/mybatis/dao/BookDao.java
+++ src/main/java/cn/meijunjie/mybatis/dao/BookDao.java
@@ -1,9 +1,11 @@
 package cn.meijunjie.mybatis.dao;
 
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
 import cn.meijunjie.mybatis.domain.Book;
 
+@Repository
 public interface BookDao {
 	@Select("select * from book where book_id=#{bookid}")
 	Book selectBookById(long bookId);
