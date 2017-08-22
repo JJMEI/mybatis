297dd77538e4c1a1dbf7f39521efe7bfb8b1f05d
meijunjie <meijunjie@le.com>
2017-08-22 18:49:38 +0800

    添加lombok 和 slf4j

--- src/main/java/cn/meijunjie/mybatis/domain/User.java
+++ src/main/java/cn/meijunjie/mybatis/domain/User.java
@@ -1,7 +1,15 @@
 package cn.meijunjie.mybatis.domain;
 
+import lombok.*;
+
 import java.util.Date;
 
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 	
 	private Integer id;
@@ -9,41 +17,7 @@ public class User {
 	private Date birthday;
 	private String sex;
 	private String address;
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public Date getBirthday() {
-		return birthday;
-	}
-	public void setBirthday(Date birthday) {
-		this.birthday = birthday;
-	}
-	public String getSex() {
-		return sex;
-	}
-	public void setSex(String sex) {
-		this.sex = sex;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", username=" + username + ", birthday=" + birthday + ", sex=" + sex + ", address="
-				+ address + "]";
-	}
+
 	
 	
 }
